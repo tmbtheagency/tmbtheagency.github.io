ac039c9407b3239eebf3147c91ba3a5d1853a7eb
ruhul0 <121794998+tmbtheagency@users.noreply.github.com>
2023-01-02 15:52:48 +0600

    init

---
+++ assets/js/scripts.js
@@ -0,0 +1,256 @@
+/*
+* ----------------------------------------------------------------------------------------
+Author       : onepageboss
+Template Name: Lia - OnePage Creative Agency Parallax template
+Version      : 1.0                                          
+* ----------------------------------------------------------------------------------------
+*/
+
+(function ($) {
+    'use strict';
+
+    jQuery(document).ready(function () {
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  PRELOADER JS
+         * ----------------------------------------------------------------------------------------
+         */
+        var prealoaderOption = $(window);
+        prealoaderOption.on("load", function () {
+            var preloader = jQuery('#loader');
+            var preloaderArea = jQuery('.preloader-area');
+            preloader.fadeOut();
+            preloaderArea.delay(350).fadeOut('slow');
+        });
+
+
+
+        $('.player').mb_YTPlayer();
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  CHANGE MENU BACKGROUND JS
+         * ----------------------------------------------------------------------------------------
+         */
+        var headertopoption = $(window);
+        var headTop = $('.header-top-area');
+
+        headertopoption.on('scroll', function () {
+            if (headertopoption.scrollTop() > 200) {
+                headTop.addClass('menu-bg');
+            } else {
+                headTop.removeClass('menu-bg');
+            }
+        });
+
+
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  SMOTH SCROOL JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        $('a.smoth-scroll').on("click", function (e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 50
+            }, 1000);
+            e.preventDefault();
+        });
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  MAGNIFIC POPUP JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+
+        $('.popup-image').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
+
+        $('.popup-video').magnificPopup({
+            type: 'iframe'
+        });
+
+
+        $('.video-play').magnificPopup({
+            type: 'iframe'
+        });
+        
+        
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  PARALLAX JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        var parallaxeffect = $(window);
+        parallaxeffect.stellar({
+            responsive: true,
+            positionProperty: 'position',
+            horizontalScrolling: false
+        });
+
+
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  PROGRESS BAR JS
+         * ----------------------------------------------------------------------------------------
+         */
+        $('.progress-bar > span').each(function () {
+            var $this = $(this);
+            var width = $(this).data('percent');
+            $this.css({
+                'transition': 'width 3s'
+            });
+            setTimeout(function () {
+                $this.appear(function () {
+                    $this.css('width', width + '%');
+                });
+            }, 500);
+        });
+
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  TESTIMONIAL JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        $(".testimonial-list").owlCarousel({
+            items: 1,
+            autoPlay: true,
+            navigation: false,
+            itemsDesktop: [1199, 1],
+            itemsDesktopSmall: [980, 1],
+            itemsTablet: [768, 1],
+            itemsTabletSmall: false,
+            itemsMobile: [479, 1],
+            pagination: true,
+            autoHeight: true,
+        });
+        
+        $(".team-list").owlCarousel({
+            items: 3,
+            autoPlay: true,
+            navigation: false,
+            itemsDesktop: [1199, 1],
+            itemsDesktopSmall: [980, 2],
+            itemsTablet: [768, 2],
+            itemsTabletSmall: false,
+            itemsMobile: [479, 2],
+            pagination: true,
+            autoHeight: true,
+        });
+
+
+
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  EXTRA JS
+         * ----------------------------------------------------------------------------------------
+         */
+        $(document).on('click', '.navbar-collapse.in', function (e) {
+            if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+                $(this).collapse('hide');
+            }
+        });
+        $('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 195
+        });
+
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  SCROOL TO UP JS
+         * ----------------------------------------------------------------------------------------
+         */
+        $(window).on("scroll", function () {
+            if ($(this).scrollTop() > 250) {
+                $('.scrollup').fadeIn();
+            } else {
+                $('.scrollup').fadeOut();
+            }
+        });
+        $('.scrollup').on("click", function () {
+            $("html, body").animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  WOW JS
+         * ----------------------------------------------------------------------------------------
+         */
+        new WOW().init();
+
+
+
+
+
+
+
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  PORTFOLIO JS
+         * ----------------------------------------------------------------------------------------
+         */
+        $('#portfolio-grid').imagesLoaded(function () {
+
+            /* Filter menu */
+            $('.portfolio-filter').on('click', 'button', function () {
+                var filterValue = $(this).attr('data-filter');
+                $grid.isotope({
+                    filter: filterValue
+                });
+            });
+
+            /* filter menu active class  */
+            $('.portfolio-filter button').on('click', function (event) {
+                $(this).siblings('.active').removeClass('active');
+                $(this).addClass('active');
+                event.preventDefault();
+            });
+
+            /* Filter active */
+            var $grid = $('#portfolio-grid').isotope({
+                itemSelector: '.portfolio-item',
+                percentPosition: true,
+                masonry: {
+                    columnWidth: '.portfolio-item',
+                }
+            });
+
+
+        });
+
+
+
+
+
+
+
+
+
+    });
+
+})(jQuery);
